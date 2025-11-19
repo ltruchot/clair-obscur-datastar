3b59760d13e8f3e7b41a911bbbc26bbf841c2822
Loïc TRUCHOT <loic.truchot@hcl-software.com>
2025-11-19 11:24:23 +0100

    dataship

---
+++ apps/dataship/main.js
@@ -0,0 +1,79 @@
+import { ServerSentEventGenerator } from '@starfederation/datastar-sdk';
+import { EventEmitter } from 'node:events';
+import { readFileSync } from 'node:fs';
+import { createServer } from 'node:http';
+
+const emitter = new EventEmitter();
+let state = {
+  fuel: 0,
+  ignition: false,
+};
+
+createServer(async (req, res) => {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
+
+  // GET HOME PAGE --> returns the HTML page
+  if (url.pathname === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.end(readFileSync('./index.html'));
+
+    // GET SUBSCRIBE TO EVENTS --> returns a SSE stream
+  } else if (url.pathname === '/subscribe-to-events') {
+    ServerSentEventGenerator.stream(
+      req,
+      res,
+      (stream) => {
+        stream.patchSignals(`{ "_fuel": ${state.fuel}, "_ignition": ${state.ignition} }`);
+
+        const listener = () => {
+          stream.patchSignals(`{ "_fuel": ${state.fuel}, "_ignition": ${state.ignition} }`);
+        };
+
+        emitter.on('fuel:changed', listener);
+
+        req.on('close', () => {
+          emitter.off('fuel:changed', listener);
+        });
+      },
+      {
+        keepalive: true,
+      },
+    );
+
+    // POST INC FUEL --> increments fuel
+  } else if (url.pathname === '/inc-fuel') {
+    state.fuel += 1;
+
+    if (state.fuel >= 100) {
+      state.ignition = true;
+      state.fuel = 0;
+      emitter.emit('fuel:changed');
+
+      setTimeout(() => {
+        state.ignition = false;
+        emitter.emit('fuel:changed');
+      }, 10000);
+    } else {
+      emitter.emit('fuel:changed');
+    }
+
+    res.writeHead(202);
+    res.end();
+    // Unknown route --> returns a 404
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+}).listen(671, () => {
+  console.log('🚀 Add fuel server running at http://localhost:671');
+});
--- presentations/datastar-2025-10-17/pages/010-what-datastar-is-not.md
+++ presentations/datastar-2025-10-17/pages/010-what-datastar-is-not.md
@@ -9,10 +9,9 @@ layout: items
   <div style="width: 75%; text-align: left;">
     <h4>Being delusional is a sin as well</h4>
     <ul>
-      <li>NOT a silver bullet - it's a solid toolbox for experienced web developers</li>
-      <li>NOT for continuous bi-directional communication (like cursor tracking)</li>
+      <li>NO silver bullet</li>
+      <li>NO continuous bi-directional communication (like cursor tracking)</li>
       <li>NOT local-first friendly</li>
-      <li>NOT for empty-HTML signals-based SPAs, NOR drag-and-drop static sites</li>
     </ul>
   </div>
   <img src="../assets/have-you-accepted.png" alt="Have you accepted?" style="display: block; width: 25%;" />
@@ -25,10 +24,10 @@ layout: items
   <div style="width: 70%; text-align: left;">
     <h4>The industry may not be ready</h4>
     <ul>
-      <li>Backend vs Frontend now mostly fulfills industry requirements...</li>
+      <li>Backend vs Frontend fulfills industry requirements...</li>
       <li>...and individual identities and skills</li>
-      <li>React-like SPAs could already be too big to fail</li>
-      <li>Metrics are rarely important in ideology-driven debates</li>
+      <li>React-like SPAs too big to fail?</li>
+      <li>Metrics are not important in ideology-driven debates</li>
     </ul>
   </div>
 </div>
