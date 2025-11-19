66bbee823af75e72146d21a0b5eebf4d94fd38de
Loïc TRUCHOT <loic.truchot@hcl-software.com>
2025-11-19 15:01:07 +0100

    dataship 3

--- apps/dataship/main.js
+++ apps/dataship/main.js
@@ -14,7 +14,7 @@ createServer(async (req, res) => {
 
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, datastar-request');
 
   if (req.method === 'OPTIONS') {
     res.writeHead(204);
